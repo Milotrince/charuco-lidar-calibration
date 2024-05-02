f2bcebbe5aa138683209bac866a7c69dc6a809ec
Trinity Chung <trinityjchung@gmail.com>
2024-05-01 19:41:00 -0700

    cleanup charuco code

--- charuco_lidar_calib/charuco.py
+++ charuco_lidar_calib/charuco.py
@@ -1,4 +1,4 @@
-import numpy as np
+from charuco_lidar_calib.camera import *
 import cv2
 
 ARUCO_DICT = cv2.aruco.DICT_4X4_250
@@ -9,7 +9,7 @@ SQUARE_LENGTH = 0.03
 MARKER_LENGTH = 0.02
 LENGTH_PX = 300  # total length of the page in pixels
 MARGIN_PX = 0  # size of the margin in pixels
-FILE_PATH = "../data/charuco/charuco_board"
+FILE_PATH = "data/charuco/charuco_board"
 NUM_BOARDS = 3
 
 boards = []
@@ -25,29 +25,71 @@ for i in range(NUM_BOARDS):
         )
     )
 
-if __name__ == "__main__":
+
+def get_board_poses(img, show=False):
+    poses = []
+    for board in boards:
+        detector = cv2.aruco.CharucoDetector(board)
+        charuco_corners, charuco_ids, marker_corners, marker_ids = detector.detectBoard(
+            img
+        )
+        if charuco_ids is not None:
+            success, rvec, tvec = cv2.aruco.estimatePoseCharucoBoard(
+                charuco_corners, charuco_ids, board, CAM_MATRIX, CAM_DIST, None, None
+            )
+            if success:
+                poses.append((rvec, tvec))
+                if show:
+                    cv2.drawFrameAxes(
+                        img, CAM_MATRIX, CAM_DIST, rvec, tvec, length=0.1, thickness=15
+                    )
+                    cv2.imshow("image", img)
+                    cv2.waitKey(0)
+    return poses
+
+
+def generate_boards():
     for board in boards:
         img = cv2.aruco.CharucoBoard.generateImage(
             board,
             (LENGTH_PX, int(LENGTH_PX * NUM_COLS / NUM_ROWS)),
             marginSize=MARGIN_PX,
         )
-        cv2.imwrite(FILE_PATH + f"-{i+1}.png", img)
+        filename = FILE_PATH + f"-{i+1}.png"
+        print("writing to", filename)
+        cv2.imwrite(filename, img)
 
-    # FOR PRINTING
 
-    # from reportlab.pdfgen import canvas
-    # from reportlab.lib.units import inch
-    # from reportlab.lib.pagesizes import letter
-    # from tempfile import NamedTemporaryFile
+def generate_boards_printable(img):
+    from reportlab.pdfgen import canvas
+    from reportlab.lib.units import inch
+    from reportlab.lib.pagesizes import letter
+    from tempfile import NamedTemporaryFile
 
-    # for i in range(len(pieces)):
-    #     with NamedTemporaryFile(suffix=".png") as f:
-    #         cv2.imwrite(f.name, pieces[i])
+    pieces = []
+    for i in range(NUM_COLS):
+        for j in range(NUM_ROWS):
+            piece = img[
+                LENGTH_PX // 3 * i : LENGTH_PX // 3 * (i + 1),
+                LENGTH_PX // 3 * j : LENGTH_PX // 3 * (j + 1),
+            ]
+            pieces.append(piece)
 
-    #         c = canvas.Canvas(f"piece-{i}.pdf", pagesize=letter)
-    #         w, h = letter
-    #         margin = 0.5 * inch
-    #         c.drawImage(f.name, margin, h-w + margin, w - 2*margin, w - 2*margin)
-    #         c.showPage()
-    #         c.save()
+    for i in range(len(pieces)):
+        with NamedTemporaryFile(suffix=".png") as f:
+            cv2.imwrite(f.name, pieces[i])
+            filename = f"{FILE_PATH}/piece-{i}.pdf"
+            print("writing to", filename)
+            c = canvas.Canvas(filename, pagesize=letter)
+            w, h = letter
+            margin = 0.5 * inch
+            c.drawImage(f.name, margin, h - w + margin, w - 2 * margin, w - 2 * margin)
+            c.showPage()
+            c.save()
+
+
+if __name__ == "__main__":
+    img = cv2.imread("data/test_img.jpg", flags=cv2.IMREAD_COLOR)
+    poses = get_board_poses(img, show=True)
+    for i, pose in enumerate(poses):
+        print(f"Board {i}: {pose}")
---
+++ charuco_lidar_calib/main.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+
+from rosbags.highlevel import AnyReader
+from rosbags.typesys import Stores, get_typestore
+import numpy as np
+import cv2
+
+from charuco_lidar_calib.charuco import get_board_poses
+
+ROSBAG_PATH = "data/cube1"
+LIDAR_TOPIC = "/luminar_front_points"
+IMAGE_TOPIC = "/vimba_front_left_center/image"
+
+bagpath = Path(ROSBAG_PATH)
+
+typestore = get_typestore(Stores.ROS2_IRON)
+
+with AnyReader([bagpath], default_typestore=typestore) as reader:
+    for connection, timestamp, rawdata in reader.messages(connections=reader.connections):
+         msg = reader.deserialize(rawdata, connection.msgtype)
+         if connection.topic == LIDAR_TOPIC:
+            print(timestamp, "lidar", msg.data)
+         if connection.topic == IMAGE_TOPIC:
+            img = msg.data
+            poses = get_board_poses(img)
+            print(timestamp, "image", poses)
