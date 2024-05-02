18ad1d7e166f9d884faecf5577a84736be564297
Trinity Chung <trinityjchung@gmail.com>
2024-05-01 21:27:49 -0700

    add reading lidar points

---
+++ charuco_lidar_calib/lidar.py
@@ -0,0 +1,69 @@
+import struct
+import math
+
+DATATYPE = ("f", 4)
+
+def _get_struct_fmt(is_bigendian, fields, field_names=None):
+    fmt = ">" if is_bigendian else "<"
+
+    offset = 0
+    for field in (
+        f
+        for f in sorted(fields, key=lambda f: f.offset)
+        if field_names is None or f.name in field_names
+    ):
+        if offset < field.offset:
+            fmt += "x" * (field.offset - offset)
+            offset = field.offset
+        else:
+            datatype_fmt, datatype_length = DATATYPE
+            fmt += field.count * datatype_fmt
+            offset += field.count * datatype_length
+
+    return fmt
+
+
+def read_points(cloud, field_names=("x", "y", "z"), skip_nans=False):
+    """
+    Read points from a L{sensor_msgs.PointCloud2} message.
+    @param cloud: The point cloud to read from.
+    @type  cloud: L{sensor_msgs.PointCloud2}
+    @param field_names: The names of fields to read. If None, read all fields. [default: None]
+    @type  field_names: iterable
+    @param skip_nans: If True, then don't return any point with a NaN value.
+    @type  skip_nans: bool [default: False]
+    @param uvs: If specified, then only return the points at the given coordinates. [default: empty list]
+    @type  uvs: iterable
+    @return: Generator which yields a list of values for each point.
+    @rtype:  generator
+    """
+    fmt = _get_struct_fmt(cloud.is_bigendian, cloud.fields, field_names)
+    width, height, point_step, row_step, data, isnan = (
+        cloud.width,
+        cloud.height,
+        cloud.point_step,
+        cloud.row_step,
+        cloud.data,
+        math.isnan,
+    )
+    unpack_from = struct.Struct(fmt).unpack_from
+
+    if skip_nans:
+        for v in range(height):
+            offset = row_step * v
+            for u in range(width):
+                p = unpack_from(data, offset)
+                has_nan = False
+                for pv in p:
+                    if isnan(pv):
+                        has_nan = True
+                        break
+                if not has_nan:
+                    yield p
+                offset += point_step
+    else:
+        for v in range(height):
+            offset = row_step * v
+            for u in range(width):
+                yield unpack_from(data, offset)
+                offset += point_step
--- charuco_lidar_calib/main.py
+++ charuco_lidar_calib/main.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 
 from charuco_lidar_calib.charuco import get_board_poses
+from charuco_lidar_calib.lidar import read_points
 
 ROSBAG_PATH = "data/rosbags/invert-cube1"
 LIDAR_TOPIC = "/luminar_front_points"
@@ -19,7 +20,8 @@ with AnyReader([bagpath], default_typestore=typestore) as reader:
     for connection, timestamp, rawdata in reader.messages(connections=reader.connections):
          msg = reader.deserialize(rawdata, connection.msgtype)
          if connection.topic == LIDAR_TOPIC:
-            print(timestamp, "lidar", msg.data)
+            points = read_points(msg)
+            print(timestamp, "lidar", list(points))
          if connection.topic == IMAGE_TOPIC:
             img = msg.data.reshape((1540, 2060, 3))
             poses = get_board_poses(img, show=False)
